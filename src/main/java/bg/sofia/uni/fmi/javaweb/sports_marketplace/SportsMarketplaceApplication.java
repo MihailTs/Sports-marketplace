643d028a1d2699a4b0976e407f0e31f18c74937f
MihailTs <mihail.tsvetkov.04@gmail.com>
2025-05-01 21:23:08 +0300

    fix issues with application start

---
+++ src/main/java/bg/sofia/uni/fmi/javaweb/sports_marketplace/SportsMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package bg.sofia.uni.fmi.javaweb.sports_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsMarketplaceApplication.class, args);
+    }
+
+}
