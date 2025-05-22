a3983a09fd5439a8e32c94c945f03e645ada2b0f
MihailTs <mihail.tsvetkov.04@gmail.com>
2025-05-22 10:54:39 +0300

    model classes

---
+++ src/main/java/bg/sofia/uni/fmi/javaweb/sports_marketplace/models/Message.java
@@ -0,0 +1,40 @@
+package bg.sofia.uni.fmi.javaweb.sports_marketplace.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "messages")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch= FetchType.LAZY)
+    @JoinColumn(name="id")
+    private Chat chat;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="id")
+    private User senderId;
+    private String content;
+    private LocalDateTime sendAt;
+    private LocalDateTime readAt;
+}
