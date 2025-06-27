8677c6233a9e67bf2fbccf1d0ca848a6bcd72e8d
MihailTs <mihail.tsvetkov.04@gmail.com>
2025-06-27 19:32:52 +0300

    small fixes and styling

--- frontend/my-app/src/app/app.routes.ts
+++ frontend/my-app/src/app/app.routes.ts
@@ -1,20 +1,29 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import {AuthGuard} from '../authGuard';
 import {ForumsPageComponent} from './forums/forums-page.component';
 import {SingleForumComponent} from './forums/single-forum.component';
-import {AddProductComponent} from './products/add-product.component';
 import {ProductsListComponent} from './products/products-list.component';
+import {AddProductComponent} from './products/add-product.component';
+import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './home/home.component';
+import {RegisterComponent} from './register/register.component';
+import {Routes} from '@angular/router';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'add-product', component: AddProductComponent },
-  { path: 'see-products', component: ProductsListComponent },
+  {
+    path: 'add-product',
+    component: AddProductComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'see-products',
+    component: ProductsListComponent,
+  },
   {
     path: 'forums',
+    canActivate: [AuthGuard],
     children: [
       { path: '', component: ForumsPageComponent },
       { path: ':id', component: SingleForumComponent }
